02bcff74d9b9e478a10c3c1e4c76e4f26d1d9e9f
OVIp9u <palagutaandrei@yandex.ru>
2023-12-09 23:02:28 +0300

    media

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
