d059ab8593a11136b5741e28862ca234a94e2810
OVIp9u <palagutaandrei@yandex.ru>
2023-12-10 00:09:51 +0300

    fn_media

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
+from django.conf.urls.static import static
 from rest_framework import routers
-
+from django.conf import settings
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router = routers.DefaultRouter()
@@ -13,3 +14,8 @@ router.register('ingredients', IngredientViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
