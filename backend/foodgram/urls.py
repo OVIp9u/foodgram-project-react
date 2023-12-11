8cf6f632854f2cc26b7a1b10d67a8105cc983e95
OVIp9u <palagutaandrei@yandex.ru>
2023-12-11 12:45:32 +0300

    media

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/', include('users.urls')),
 ]
 
-if settings.DEBUG:
+'''if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    )'''
